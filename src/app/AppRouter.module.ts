e5faef89c31d3f0ac2300543b993f0f202876785
benjaminbascary <benjaminbascary@gmail.com>
2022-08-18 17:42:47 -0300

    Creates main routing features

---
+++ src/app/AppRouter.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { NotfoundComponent } from './shared/notfound/notfound.component';
+
+const routes: Routes = [
+  {
+    path: 'notfound',
+    component: NotfoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'notfound'
+  }
+]
+
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRouterModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+// Styling
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// Developer's
+import { AppComponent } from './app.component';
 import { NotfoundComponent } from './shared/notfound/notfound.component';
+import { AppRouterModule } from './AppRouter.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { NotfoundComponent } from './shared/notfound/notfound.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AppRouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
